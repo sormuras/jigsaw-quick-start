86866325c06413e6a9610d1a9b3c8c0d6c546970
Christian Stein <christian.s.stein@oracle.com>
2024-07-21 07:28:34 +0300

    Override default start method

--- b2/src/Project.java
+++ b2/src/Project.java
@@ -4,4 +4,11 @@ record Project(Model model) implements Builder, Cleaner, Rebuilder, Starter {
   static Project ofCurrentWorkingDirectory() {
     return new Project(Model.of("b2"));
   }
+
+  @Override
+  public void start() {
+    System.out.println("BEGIN");
+    Starter.super.start();
+    System.out.println("END.");
+  }
 }
